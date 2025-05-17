50865b740bc5aa85d8a58bcecad684da721b10e5
straysheep14 <aidaneileen123@gmail.com>
2025-05-16 20:43:23 -0700

    Pre bike lane

--- map.js
+++ map.js
@@ -1,14 +1,18 @@
+// Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWF2ZXJldHQiLCJhIjoiY21hbzhjenN0MDJkdjJrbzhrZGc5cGw0MCJ9.2d9tcLesxsnfETG1MF3NGQ';
-
+// Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWF2ZXJldHQiLCJhIjoiY21hbzhjenN0MDJkdjJrbzhrZGc5cGw0MCJ9.2d9tcLesxsnfETG1MF3NGQ';
+
+// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', 
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-71.09415, 42.36027], 
-  zoom: 12,
-  minZoom: 5, 
-  maxZoom: 18 
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
 });
